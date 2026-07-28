08be2226f28e74701522a1ff67024fcb0e28337a
Veiir Li <veiir.li@yorun.ai>
2026-07-29 01:30:56 +0800

    fix: share site preferences and serve Markdown as UTF-8 (#16)
    * fix: serve markdown as UTF-8
    
    * feat: share site preferences across subdomains

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,6 +86,7 @@ const config: Config = {
     ['./src/plugins/tailwind.ts', {}],
     ['./src/plugins/aliases.ts', {}],
     ['./src/plugins/markdown-pages.ts', {}],
+    ['./src/plugins/shared-preferences.ts', {}],
     ...(devWebSocketPath
       ? [
           () => ({
